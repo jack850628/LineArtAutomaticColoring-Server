ee34624d8b7bf2327d153bd7558e6db3e62f6df5
jack850628 <jack850628@gmail.com>
2022-10-07 20:41:00 +0800

    修正圖片輸入時的預處理，讓模型發揮應有的水準

--- flaskr/Model/Pix2pix.py
+++ flaskr/Model/Pix2pix.py
@@ -1,7 +1,7 @@
 from keras.models import load_model
 from tensorflow.keras.preprocessing.image import  img_to_array, array_to_img
 from PIL import Image
-
+import tensorflow as tf
 import numpy as np
 import base64, io
 
@@ -19,13 +19,32 @@ def png2jpg(image):
 
     return composited
 
+def imgResize(img, imageSize=256):
+    if img.size[0] > img.size[1]:
+        newHeight = int(img.size[1] * imageSize / img.size[0])
+        nimg = img.resize((imageSize, newHeight))
+    else:
+        newWidth = int(img.size[0] * imageSize / img.size[1])
+        nimg = img.resize((newWidth, imageSize))
+    
+    nimg2 = Image.new(nimg.mode, (imageSize, imageSize), (255, 255, 255))
+    if(nimg.size[0] > nimg.size[1]):
+        nimg2.paste(nimg, (0, (imageSize - nimg.size[1]) // 2))
+    else:
+        nimg2.paste(nimg, ((imageSize - nimg.size[0]) // 2, 0))
+    return nimg2
+
 def create(str):
     str = str.split(',')[1]
     str = base64.b64decode(str)
     buf = io.BytesIO(str)
     image = Image.open(buf)
-    image = png2jpg(image)
+    # image.convert('RGB').save('./flaskr/checkpoint/test.jpg')
+    image = imgResize(image)
+    # image = png2jpg(image)
     image = img_to_array(image)
+    image = (image / 127.5) - 1
+    image = tf.cast(image, tf.float32)
     image = model(np.array([image]), training=True)
     image = array_to_img(image[0])
     output_buffer = io.BytesIO()
